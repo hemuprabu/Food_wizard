9b9c021da5e298e703691269fd8713b3f8fa27aa
hemuprabu <hemalathaprakasam219@gmail.com>
2024-04-30 12:03:20 -0400

    updated login and signup page

---
+++ controllers/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const signupRoutes= require('./signupRoutes');
+
+router.use('/users', userRoutes);
+router.use('/signup', signupRoutes );
+module.exports = router;
